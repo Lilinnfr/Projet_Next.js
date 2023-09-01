53587935f54d2673e46c2f5c838c88d699beaf1b
Lilinnfr <zelyly77@hotmail.fr>
2023-09-01 16:51:23 +0200

    add error page

---
+++ pages/404.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from '@/styles/Home.module.css'
+
+export default function Error404() {
+  return (
+    <div className={styles.container}>404 ! La fameuse :)</div>
+  )
+}
