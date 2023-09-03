cbef7fe788a0d20bed1d74fcff6a9e20c40bffa9
Lilinnfr <zelyly77@hotmail.fr>
2023-09-03 13:37:57 +0200

    change structure + project

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 
 import styles from '@/styles/Home.module.css'
-import Link from 'next/link'
 import Head from 'next/head'
 
 
@@ -8,13 +7,11 @@ export default function Home() {
   return (
     <>
       <Head>
-          <meta charset="UTF-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <title>Les bons p'tits plats</title>
+          <title>Titre</title>
       </Head>
       <div className={styles.container}>
         <h1>Next.js</h1>
-        <button class="btn btn-primary">Bouton</button>
       </div>
     </>
   )
